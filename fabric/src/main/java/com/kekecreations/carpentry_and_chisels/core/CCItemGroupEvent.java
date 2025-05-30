2913af450f4d3ff1542ef27b8be2a0bd117e6eca
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2025-05-30 03:46:20 +0100

    Wood slabs finished

--- fabric/src/main/java/com/kekecreations/carpentry_and_chisels/core/CCItemGroupEvent.java
+++ fabric/src/main/java/com/kekecreations/carpentry_and_chisels/core/CCItemGroupEvent.java
@@ -17,7 +17,7 @@ public class CCItemGroupEvent {
             event.addAfter(Blocks.OAK_SLAB, CCBlocks.CARVED_OAK_WOOD.get());
             event.addAfter(Blocks.DARK_OAK_SLAB, CCBlocks.CARVED_DARK_OAK_WOOD.get());
             event.addAfter(Blocks.ACACIA_SLAB, CCBlocks.CARVED_ACACIA_WOOD.get());
-            event.addAfter(Blocks.BAMBOO_SLAB, CCBlocks.CARVED_BAMBOO_WOOD.get());
+            event.addAfter(Blocks.BAMBOO_MOSAIC_SLAB, CCBlocks.CARVED_BAMBOO_WOOD.get());
             event.addAfter(Blocks.BIRCH_SLAB, CCBlocks.CARVED_BIRCH_WOOD.get());
             event.addAfter(Blocks.CHERRY_SLAB, CCBlocks.CARVED_CHERRY_WOOD.get());
             event.addAfter(Blocks.CRIMSON_SLAB, CCBlocks.CARVED_CRIMSON_WOOD.get());
@@ -27,9 +27,29 @@ public class CCItemGroupEvent {
             event.addAfter(Blocks.WARPED_SLAB, CCBlocks.CARVED_WARPED_WOOD.get());
 
             event.addAfter(Blocks.OAK_WOOD, CCBlocks.OAK_LOG_SLAB.get());
+            event.addAfter(Blocks.DARK_OAK_WOOD, CCBlocks.DARK_OAK_LOG_SLAB.get());
+            event.addAfter(Blocks.ACACIA_WOOD, CCBlocks.ACACIA_LOG_SLAB.get());
+            event.addAfter(Blocks.BAMBOO_BLOCK, CCBlocks.BAMBOO_BLOCK_SLAB.get());
+            event.addAfter(Blocks.BIRCH_WOOD, CCBlocks.BIRCH_LOG_SLAB.get());
+            event.addAfter(Blocks.CHERRY_WOOD, CCBlocks.CHERRY_LOG_SLAB.get());
+            event.addAfter(Blocks.CRIMSON_HYPHAE, CCBlocks.CRIMSON_STEM_SLAB.get());
+            event.addAfter(Blocks.JUNGLE_WOOD, CCBlocks.JUNGLE_LOG_SLAB.get());
+            event.addAfter(Blocks.MANGROVE_WOOD, CCBlocks.MANGROVE_LOG_SLAB.get());
+            event.addAfter(Blocks.SPRUCE_WOOD, CCBlocks.SPRUCE_LOG_SLAB.get());
+            event.addAfter(Blocks.WARPED_HYPHAE, CCBlocks.WARPED_STEM_SLAB.get());
 
 
             event.addAfter(Blocks.STRIPPED_OAK_WOOD, CCBlocks.STRIPPED_OAK_LOG_SLAB.get());
+            event.addAfter(Blocks.STRIPPED_DARK_OAK_WOOD, CCBlocks.STRIPPED_DARK_OAK_LOG_SLAB.get());
+            event.addAfter(Blocks.STRIPPED_ACACIA_WOOD, CCBlocks.STRIPPED_ACACIA_LOG_SLAB.get());
+            event.addAfter(Blocks.STRIPPED_BAMBOO_BLOCK, CCBlocks.STRIPPED_BAMBOO_BLOCK_SLAB.get());
+            event.addAfter(Blocks.STRIPPED_BIRCH_WOOD, CCBlocks.STRIPPED_BIRCH_LOG_SLAB.get());
+            event.addAfter(Blocks.STRIPPED_CHERRY_WOOD, CCBlocks.STRIPPED_CHERRY_LOG_SLAB.get());
+            event.addAfter(Blocks.STRIPPED_CRIMSON_HYPHAE, CCBlocks.STRIPPED_CRIMSON_STEM_SLAB.get());
+            event.addAfter(Blocks.STRIPPED_JUNGLE_WOOD, CCBlocks.STRIPPED_JUNGLE_LOG_SLAB.get());
+            event.addAfter(Blocks.STRIPPED_MANGROVE_WOOD, CCBlocks.STRIPPED_MANGROVE_LOG_SLAB.get());
+            event.addAfter(Blocks.STRIPPED_SPRUCE_WOOD, CCBlocks.STRIPPED_SPRUCE_LOG_SLAB.get());
+            event.addAfter(Blocks.STRIPPED_WARPED_HYPHAE, CCBlocks.STRIPPED_WARPED_STEM_SLAB.get());
         });
     }
 }
--- forge/src/main/java/com/kekecreations/carpentry_and_chisels/ForgeCarpentryAndChisels.java
+++ forge/src/main/java/com/kekecreations/carpentry_and_chisels/ForgeCarpentryAndChisels.java
@@ -58,7 +58,7 @@ public class ForgeCarpentryAndChisels {
             addAfter(event, Blocks.OAK_SLAB, CCBlocks.CARVED_OAK_WOOD.get());
             addAfter(event, Blocks.DARK_OAK_SLAB, CCBlocks.CARVED_DARK_OAK_WOOD.get());
             addAfter(event, Blocks.ACACIA_SLAB, CCBlocks.CARVED_ACACIA_WOOD.get());
-            addAfter(event, Blocks.BAMBOO_SLAB, CCBlocks.CARVED_BAMBOO_WOOD.get());
+            addAfter(event, Blocks.BAMBOO_MOSAIC_SLAB, CCBlocks.CARVED_BAMBOO_WOOD.get());
             addAfter(event, Blocks.BIRCH_SLAB, CCBlocks.CARVED_BIRCH_WOOD.get());
             addAfter(event, Blocks.CHERRY_SLAB, CCBlocks.CARVED_CHERRY_WOOD.get());
             addAfter(event, Blocks.CRIMSON_SLAB, CCBlocks.CARVED_CRIMSON_WOOD.get());
@@ -66,6 +66,33 @@ public class ForgeCarpentryAndChisels {
             addAfter(event, Blocks.MANGROVE_SLAB, CCBlocks.CARVED_MANGROVE_WOOD.get());
             addAfter(event, Blocks.SPRUCE_SLAB, CCBlocks.CARVED_SPRUCE_WOOD.get());
             addAfter(event, Blocks.WARPED_SLAB, CCBlocks.CARVED_WARPED_WOOD.get());
+
+
+
+            addAfter(event, Blocks.OAK_WOOD, CCBlocks.OAK_LOG_SLAB.get());
+            addAfter(event, Blocks.DARK_OAK_WOOD, CCBlocks.DARK_OAK_LOG_SLAB.get());
+            addAfter(event, Blocks.ACACIA_WOOD, CCBlocks.ACACIA_LOG_SLAB.get());
+            addAfter(event, Blocks.BAMBOO_BLOCK, CCBlocks.BAMBOO_BLOCK_SLAB.get());
+            addAfter(event, Blocks.BIRCH_WOOD, CCBlocks.BIRCH_LOG_SLAB.get());
+            addAfter(event, Blocks.CHERRY_WOOD, CCBlocks.CHERRY_LOG_SLAB.get());
+            addAfter(event, Blocks.CRIMSON_HYPHAE, CCBlocks.CRIMSON_STEM_SLAB.get());
+            addAfter(event, Blocks.JUNGLE_WOOD, CCBlocks.JUNGLE_LOG_SLAB.get());
+            addAfter(event, Blocks.MANGROVE_WOOD, CCBlocks.MANGROVE_LOG_SLAB.get());
+            addAfter(event, Blocks.SPRUCE_WOOD, CCBlocks.SPRUCE_LOG_SLAB.get());
+            addAfter(event, Blocks.WARPED_HYPHAE, CCBlocks.WARPED_STEM_SLAB.get());
+
+
+            addAfter(event, Blocks.STRIPPED_OAK_WOOD, CCBlocks.STRIPPED_OAK_LOG_SLAB.get());
+            addAfter(event, Blocks.STRIPPED_DARK_OAK_WOOD, CCBlocks.STRIPPED_DARK_OAK_LOG_SLAB.get());
+            addAfter(event, Blocks.STRIPPED_ACACIA_WOOD, CCBlocks.STRIPPED_ACACIA_LOG_SLAB.get());
+            addAfter(event, Blocks.STRIPPED_BAMBOO_BLOCK, CCBlocks.STRIPPED_BAMBOO_BLOCK_SLAB.get());
+            addAfter(event, Blocks.STRIPPED_BIRCH_WOOD, CCBlocks.STRIPPED_BIRCH_LOG_SLAB.get());
+            addAfter(event, Blocks.STRIPPED_CHERRY_WOOD, CCBlocks.STRIPPED_CHERRY_LOG_SLAB.get());
+            addAfter(event, Blocks.STRIPPED_CRIMSON_HYPHAE, CCBlocks.STRIPPED_CRIMSON_STEM_SLAB.get());
+            addAfter(event, Blocks.STRIPPED_JUNGLE_WOOD, CCBlocks.STRIPPED_JUNGLE_LOG_SLAB.get());
+            addAfter(event, Blocks.STRIPPED_MANGROVE_WOOD, CCBlocks.STRIPPED_MANGROVE_LOG_SLAB.get());
+            addAfter(event, Blocks.STRIPPED_SPRUCE_WOOD, CCBlocks.STRIPPED_SPRUCE_LOG_SLAB.get());
+            addAfter(event, Blocks.STRIPPED_WARPED_HYPHAE, CCBlocks.STRIPPED_WARPED_STEM_SLAB.get());
         }
     }
 }
